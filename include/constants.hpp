c9510aecda2a73280dbc0fa285795fee8a92f7ef
David Monk <dm2614@ic.ac.uk>
2018-10-19 14:41:48 +0100

    pushed to lx

--- include/constants.hpp
+++ include/constants.hpp
@@ -3,7 +3,7 @@
 #define TRACK_SIZE 16
 #define NUMBER_OF_LAYERS 8
 #define PI 3.14159265359
-#define V_INIT 0.0045
+#define V_INIT 0.0048
 #define V_ALPHA 0.001
 #define P_ALPHA 0.15
 #define ERROR_THRESHOLD 0.04//0.017//0.01
