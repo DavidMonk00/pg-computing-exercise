ada69e75f5c49220c926d26c43b202ff3ada656d
David Monk <dm2614@ic.ac.uk>
2018-10-19 14:40:33 +0100

    pushed to lx

--- include/constants.hpp
+++ include/constants.hpp
@@ -6,7 +6,7 @@
 #define V_INIT 0.0045
 #define V_ALPHA 0.001
 #define P_ALPHA 0.15
-#define ERROR_THRESHOLD 0.04//0.017//0.01
+#define ERROR_THRESHOLD 0.06//0.017//0.01
 #define FILENAME "./data/manytracks.raw"
 #define MAX_ITERATIONS 500
 #define TOTAL_TRACKS 1e6
